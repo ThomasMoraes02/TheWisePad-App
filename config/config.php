69a7784a4fa0263510b40e576e05f908eec39e12
Thomas Moraes <tho.vini7@gmail.com>
2022-11-03 12:38:36 -0300

    Configuracao do app

--- config/config.php
+++ config/config.php
@@ -1,20 +1,26 @@
 <?php
 
+use TheWisePad\Infraestructure\Note\NoteRepositoryMongodb;
+use TheWisePad\Infraestructure\PasswordArgonII;
+use TheWisePad\Infraestructure\TokenJWT;
+use TheWisePad\Infraestructure\User\UserRepositoryMongodb;
+
+// Display Errors
 // ini_set('display_errors', 1);
 
 // Config base path
 define("BASE_PATH", "/projetos/thewisepad");
 
 // Config Encoder Password
-define("ENCODER", "TheWisePad\Infraestructure\PasswordArgonII");
+define("ENCODER", PasswordArgonII::class);
 
 // Config Repository
-define("USER_REPOSITORY", "TheWisePad\Infraestructure\User\UserRepositoryMongodb");
+define("USER_REPOSITORY", UserRepositoryMongodb::class);
 
-define("NOTE_REPOSITORY", "TheWisePad\Infraestructure\Note\NoteRepositoryMongodb");
+define("NOTE_REPOSITORY", NoteRepositoryMongodb::class);
 
 // Config Token Manager
-define("TOKEN_MANAGER", "TheWisePad\Infraestructure\TokenJWT");
+define("TOKEN_MANAGER", TokenJWT::class);
 
 define("JWT_SECRET_TOKEN", "token-jwt-thewisedev");
 
